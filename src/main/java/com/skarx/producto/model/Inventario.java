2c3f124e039605a820e2d29ab7fc07506aa501df
marceloriv <ma.riveracontreras@duocuc.cl>
2025-05-27 02:50:25 -0400

    Reorganiza importaciones y corrige tipo de dato en el campo id de Inventario; añade anotaciones de entidad y ajusta el orden de los atributos.

--- src/main/java/com/skarx/producto/model/Inventario.java
+++ src/main/java/com/skarx/producto/model/Inventario.java
@@ -3,28 +3,30 @@ package com.skarx.producto.model;
 import java.time.LocalDate;
 import java.util.List;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
+@Data
 public class Inventario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @Column(nullable = false)
     private LocalDate fechaActualizacion;
-
-    @OneToMany(mappedBy = "inventario", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Producto> productos;
 
     @ManyToOne
     @JoinColumn(name = "id_proveedor", nullable = false)
     private Proveedor proveedor;
+
+    @OneToMany(mappedBy = "inventario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Producto> productos;
 }
