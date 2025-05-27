3453011472e9edeef5b06fa3c28eee2511c89339
marceloriv <ma.riveracontreras@duocuc.cl>
2025-05-27 02:50:41 -0400

    Elimina métodos innecesarios en RepositoryProducto y simplifica la interfaz.

--- src/main/java/com/skarx/producto/repository/RepositoryProducto.java
+++ src/main/java/com/skarx/producto/repository/RepositoryProducto.java
@@ -4,17 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.skarx.producto.model.Producto;
-import java.util.List;
 
 @Repository
 public interface RepositoryProducto extends JpaRepository<Producto, Long> {
 
-    Producto crearProducto(Producto producto);
-
     Producto findById(long id);
 
     Producto findByNombre(String nombre);
-
-    List<Producto> obtenerProductos();
 
 }
