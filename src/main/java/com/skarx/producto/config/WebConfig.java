d2cbf2050fc1ea5141fa74d8ca2767b601b88e70
marceloriv <ma.riveracontreras@duocuc.cl>
2025-12-03 03:40:38 -0300

    fix: Actualizar configuración CORS para permitir patrones de origen y métodos dinámicos

--- src/main/java/com/skarx/producto/config/WebConfig.java
+++ src/main/java/com/skarx/producto/config/WebConfig.java
@@ -14,9 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
